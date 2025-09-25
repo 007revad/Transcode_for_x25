a442c4abb5f3d317ab070e635c8e99d4ca298c32
Dave Russell <39733752+007revad@users.noreply.github.com>
2025-09-25 13:03:50 +1000

    Update script version and implement autoupdate
    Updated script version to v1.1.0 and added autoupdate feature.

--- transcode_for_x25.sh
+++ transcode_for_x25.sh
@@ -4,7 +4,7 @@
 # https://www.blackvoid.club/content/files/2025/09/x25_hw_transcode_modules.zip
 #----------------------------------------------------------------------------------
 
-scriptver="v1.0.0"
+scriptver="v1.1.0"
 script=Transcode_for_x25
 repo="007revad/Transcode_for_x25"
 scriptname=transcode_for_x25
@@ -20,6 +20,11 @@ Cyan='\e[0;36m'     # ${Cyan}
 #White='\e[0;37m'   # ${White}
 Error='\e[41m'      # ${Error}
 Off='\e[0m'         # ${Off}
+
+Cyan=""
+Error=""
+Off=""
+
 
 ding(){ 
     printf \\a
@@ -63,21 +68,126 @@ echo "$model DSM $productversion-$buildnumber$smallfix $buildphase"
 # Show CPU arch and platform_name
 echo "CPU $platform_name $arch"
 
-#------------------------------------------------------------------------------
-# Check latest release with GitHub API
+
+# Show options used
+if [[ ${#args[@]} -gt "0" ]]; then
+    echo -e "Using options: ${args[*]}\n"
+else
+    echo ""
+fi
+
+
+usage(){ 
+    cat <<EOF
+Usage: $(basename "$0") [options]
+
+Options:
+  -h, --help            Show this help message
+  -v, --version         Show the script version
+      --autoupdate=AGE  Auto update script (useful when script is scheduled)
+                          AGE is how many days old a release must be before
+                          auto-updating. AGE must be a number: 0 or greater
+
+EOF
+    exit 0
+}
+
+scriptversion(){ 
+    cat <<EOF
+$script $scriptver - by 007revad
+
+See https://github.com/$repo
+EOF
+    exit 0
+}
+
 
 # Save options used
 args=("$@")
 
+
+autoupdate=""
+
+# Check for flags with getopt
+if options="$(getopt -o abcdefghijklmnopqrstuvwxyz0123456789 -l \
+    help,version,autoupdate:,log,debug -- "$@")"; then
+    eval set -- "$options"
+    while true; do
+        case "${1,,}" in
+            -h|--help)          # Show usage options
+                usage
+                ;;
+            -v|--version)       # Show script version
+                scriptversion
+                ;;
+            -l|--log)           # Log (currently unused)
+                log=yes
+                ;;
+            -d|--debug)         # Show and log debug info (currently unused)
+                debug=yes
+                ;;
+            --autoupdate)       # Auto update script
+                autoupdate=yes
+                if [[ $2 =~ ^[0-9]+$ ]]; then
+                    delay="$2"
+                    shift
+                else
+                    delay="0"
+                fi
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)                  # Show usage options
+                ding
+                echo -e "Invalid option '$1'\n"
+                usage
+                exit 2  # Invalid argument
+                ;;
+        esac
+        shift
+    done
+else
+    echo
+    usage
+fi
+
+
+#------------------------------------------------------------------------------
+# Check latest release with GitHub API
+
+syslog_set(){ 
+    if [[ ${1,,} == "info" ]] || [[ ${1,,} == "warn" ]] || [[ ${1,,} == "err" ]]; then
+        if [[ $autoupdate == "yes" ]]; then
+            # Add entry to Synology system log
+            /usr/syno/bin/synologset1 sys "$1" 0x11100000 "$2"
+        fi
+    fi
+}
+
+
 # Get latest release info
 # Curl timeout options:
 # https://unix.stackexchange.com/questions/94604/does-curl-have-a-timeout
-release=$(curl --silent --connect-timeout 30 \
+release=$(curl --silent -m 10 --connect-timeout 5 \
     "https://api.github.com/repos/$repo/releases/latest")
 
 # Release version
 tag=$(echo "$release" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 shorttag="${tag:1}"
+
+# Release published date
+published=$(echo "$release" | grep '"published_at":' | sed -E 's/.*"([^"]+)".*/\1/')
+published="${published:0:10}"
+published=$(date -d "$published" '+%s')
+
+# Today's date
+now=$(date '+%s')
+
+# Days since release published
+age=$(((now - published)/(60*60*24)))
+
 
 # Get script location
 # https://stackoverflow.com/questions/59895/
@@ -102,11 +212,14 @@ echo "Running from: ${scriptpath}/$scriptfile"
 
 
 cleanup_tmp(){ 
+    cleanup_err=
+
     # Delete downloaded .tar.gz file
     if [[ -f "/tmp/$script-$shorttag.tar.gz" ]]; then
         if ! rm "/tmp/$script-$shorttag.tar.gz"; then
             echo -e "${Error}ERROR${Off} Failed to delete"\
                 "downloaded /tmp/$script-$shorttag.tar.gz!" >&2
+            cleanup_err=1
         fi
     fi
 
@@ -115,7 +228,13 @@ cleanup_tmp(){
         if ! rm -r "/tmp/$script-$shorttag"; then
             echo -e "${Error}ERROR${Off} Failed to delete"\
                 "downloaded /tmp/$script-$shorttag!" >&2
+            cleanup_err=1
         fi
+    fi
+
+    # Add warning to DSM log
+    if [[ $cleanup_err ]]; then
+        syslog_set warn "$script update failed to delete tmp files"
     fi
 }
 
@@ -134,8 +253,18 @@ if ! printf "%s\n%s\n" "$tag" "$scriptver" |
         echo "You have the latest version extracted but are using an older version"
         sleep 10
     else
-        echo -e "${Cyan}Do you want to download $tag now?${Off} [y/n]"
-        read -r -t 30 reply
+        if [[ $autoupdate == "yes" ]]; then
+            if [[ $age -gt "$delay" ]] || [[ $age -eq "$delay" ]]; then
+                echo "Downloading $tag"
+                reply=y
+            else
+                echo "Skipping as $tag is less than $delay days old."
+            fi
+        else
+            echo -e "${Cyan}Do you want to download $tag now?${Off} [y/n]"
+            read -r -t 30 reply
+        fi
+
         if [[ ${reply,,} == "y" ]]; then
             # Delete previously downloaded .tar.gz file and extracted tmp files
             cleanup_tmp
@@ -145,17 +274,20 @@ if ! printf "%s\n%s\n" "$tag" "$scriptver" |
                 if ! curl -JLO -m 30 --connect-timeout 5 "$url"; then
                     echo -e "${Error}ERROR${Off} Failed to download"\
                         "$script-$shorttag.tar.gz!"
+                    syslog_set warn "$script $tag failed to download"
                 else
                     if [[ -f /tmp/$script-$shorttag.tar.gz ]]; then
                         # Extract tar file to /tmp/<script-name>
                         if ! tar -xf "/tmp/$script-$shorttag.tar.gz" -C "/tmp"; then
                             echo -e "${Error}ERROR${Off} Failed to"\
                                 "extract $script-$shorttag.tar.gz!"
+                            syslog_set warn "$script failed to extract $script-$shorttag.tar.gz!"
                         else
                             # Set script sh files as executable
                             if ! chmod a+x "/tmp/$script-$shorttag/"*.sh ; then
                                 permerr=1
                                 echo -e "${Error}ERROR${Off} Failed to set executable permissions"
+                                syslog_set warn "$script failed to set permissions on $tag"
                             fi
 
                             # Copy new script sh file to script location
@@ -164,6 +296,7 @@ if ! printf "%s\n%s\n" "$tag" "$scriptver" |
                                 copyerr=1
                                 echo -e "${Error}ERROR${Off} Failed to copy"\
                                     "$script-$shorttag sh file(s) to:\n $scriptpath/${scriptfile}"
+                                syslog_set warn "$script failed to copy $tag to script location"
                             fi
 
                             # Copy new CHANGES.txt file to script location (if script on a volume)
@@ -179,6 +312,7 @@ if ! printf "%s\n%s\n" "$tag" "$scriptver" |
                                 if ! cp -p "/tmp/$script-$shorttag/CHANGES.txt"\
                                     "${scriptpath}/${scriptname}_CHANGES.txt";
                                 then
+                                    if [[ $autoupdate != "yes" ]]; then copyerr=1; fi
                                     echo -e "${Error}ERROR${Off} Failed to copy"\
                                         "$script-$shorttag/CHANGES.txt to:\n $scriptpath"
                                 else
@@ -192,21 +326,26 @@ if ! printf "%s\n%s\n" "$tag" "$scriptver" |
                             # Notify of success (if there were no errors)
                             if [[ $copyerr != 1 ]] && [[ $permerr != 1 ]]; then
                                 echo -e "\n$tag ${scriptfile}$changestxt downloaded to: ${scriptpath}\n"
+                                syslog_set info "$script successfully updated to $tag"
 
                                 # Reload script
                                 printf -- '-%.0s' {1..79}; echo  # print 79 -
                                 exec "${scriptpath}/$scriptfile" "${args[@]}"
+                            else
+                                syslog_set warn "$script update to $tag had errors"
                             fi
                         fi
                     else
                         echo -e "${Error}ERROR${Off}"\
                             "/tmp/$script-$shorttag.tar.gz not found!"
                         #ls /tmp | grep "$script"  # debug
+                        syslog_set warn "/tmp/$script-$shorttag.tar.gz not found"
                     fi
                 fi
                 cd "$scriptpath" || echo -e "${Error}ERROR${Off} Failed to cd to script location!"
             else
                 echo -e "${Error}ERROR${Off} Failed to cd to /tmp!"
+                syslog_set warn "$script update failed to cd to /tmp"
             fi
         fi
     fi
